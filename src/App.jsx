ce531cf1f50167aa8add2f6a6cab557cf018cbaf
dehuihu <dehuihu@gmail.com>
2025-06-14 07:29:51 -0700

    Implemented displaying total likes in Wave_03

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,19 @@ import { useState } from 'react';
 const senderA = ENTRIES[0].sender;
 const senderB = ENTRIES[0].sender;
 
+// define function to calculate total likes
+const calculateTotalLikes = (entries) => {
+  return entries.reduce((total, entry) => {
+    return entry.liked? total+1 : total;
+  }, 0);
+};
 const App = () => {
   // add toggleLiked event handler
   // register entries as state variable, loop through entries, to find matching id, toggle liked status;
   const [entries, setEntries] = useState(ENTRIES);
+  const totalLikes = calculateTotalLikes(entries);
+
+  console.log(totalLikes);
   const toggleLiked = (id) => {
     setEntries(entries.map((entry) => {
       if (entry.id === id) {
@@ -23,7 +32,7 @@ const App = () => {
     <div id="App">
       <header>
         <h1>Chat Between {senderA} and {senderB}</h1>
-        <p>0❤️s</p>
+        <p>{totalLikes}❤️s</p>
       </header>
       <main>
         <ChatLog
