e697f647d05a30848d82b51a0aa6f15bc67a7c81
dehuihu <dehuihu@gmail.com>
2025-06-14 23:27:57 -0700

    Fix issues with loading jsx file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'classic' // Add this line
+  })],
+  base: '/react-chatlog/',  // repo name (critical for GitHub Pages)
   test: {
     // jest config here
     reporters: ['verbose'],
